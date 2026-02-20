9c5556f4c5a50ee513145ea4d0233dd315e39d2b
Manan Vasani <mananvasani801@gmail.com>
2026-02-20 13:50:28 +0530

    Reset tracking, apply .gitignore

--- backend/routes/menuRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllItems, getItemById } = require("../controllers/menuController");
-
-router.get("/", getAllItems);
-router.get("/:id", getItemById);
-
-module.exports = router;
--- backend/routes/reservationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createReservation, getMyReservations } = require("../controllers/reservationController");
-const { protect } = require("../middleware/auth");
-
-router.post("/", protect, createReservation);
-router.get("/my", protect, getMyReservations);
-
-module.exports = router;
